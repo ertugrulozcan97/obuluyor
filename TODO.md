4c3876b1b9d6a4a9842fb7fea9da838b55e83add
ertugrulozcan <ertugrulozcan97@gmail.com>
2024-08-29 21:14:07 +0300

    update

---
+++ TODO.md
@@ -0,0 +1,12 @@
+- Layout
+    - [x] Header
+    - [ ] Footer
+
+- Genel
+    - [ ] Üye Ol
+    - [ ] Üye Girişi
+
+- Profilim
+    - [ ] Profil Bilgileri
+    - [ ] Şifre Değiştir
+    - [ ] Mesajlarım
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// Tab Navigation
 $(document).on('click', '.tab-nav-item', function() {
     const target = $($(this).data('tab-target'));
 
@@ -7,3 +8,47 @@ $(document).on('click', '.tab-nav-item', function() {
     target.siblings().removeClass('active');
     target.addClass('active');
 });
+
+// Modal
+$(document).on('click', '[data-toggle="modal"]', function(e) {
+    e.preventDefault();
+
+    const target = $($(this).data('target'));
+
+    toggleModal(target);
+});
+
+$(document).on('click', '[data-modal-close]', function() {
+    const target = $($(this).closest('.modal'));
+
+    toggleModal(target);
+});
+
+function toggleModal(target)
+{
+    // Check if the modal is already open
+    if (!target.hasClass('active'))
+    {
+        $('body').append('<div class="modal-backdrop"></div>');
+
+        setTimeout(() => {
+            $('.modal-backdrop').addClass('show');
+
+            target.fadeIn(250).addClass('active');
+        }, 250);
+
+        $('html').addClass('page-scroll-lock');
+    }
+    else
+    {
+        target.fadeOut(250).removeClass('active');
+
+        setTimeout(() => {
+            $('.modal-backdrop').remove();
+
+            target.removeClass('active');
+        }, 250);
+
+        $('html').removeClass('page-scroll-lock');
+    }
+}
